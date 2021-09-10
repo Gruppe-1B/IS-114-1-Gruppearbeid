d0d52d5127d9a6297bbd5164c67ed84d8bb02814
DomNook <ddd365rocks@gmail.com>
2021-09-10 12:07:37 +0200

    skrevet pyton script

---
+++ Dominykas/dominykas.py
@@ -0,0 +1,13 @@
+
+variabel1 = "first"
+variabel2 = "second"
+
+print(variabel1)
+print(variabel2)
+
+for element in range (10):
+    if element >= 5:
+        print (element)
+        print (variabel1)
+    else:
+        print ("not 5 yet")
