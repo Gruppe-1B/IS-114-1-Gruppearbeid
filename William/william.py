4efe1b8727b49ef1497ee1ad0f16978d41a850e4
William Peter Åredal <williamaredal@hotmail.com>
2021-09-10 12:09:24 +0200

    scriptet

---
+++ William/william.py
@@ -0,0 +1,18 @@
+
+variabel1 = "first"
+variabel2 = "second"
+
+print(variabel1)
+print(variabel2)
+
+for element in range(10):
+    if element >= 5:
+        print(element)
+        print(variabel1)
+
+    else:
+        print("ikke på 5 ennå")
+
+
+
+
